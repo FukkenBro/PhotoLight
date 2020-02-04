5b55d9c5071a6f9fe94e85f4afec0695f9241099
Paul <stars.sun.moon@mail.ru>
2020-02-04 04:08:22 +0200

    Added final func to button debounce and long\short press processing. Final version

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@
 #define MAX_BRIGHTNES 255
 // тип светодиодной ленты
 struct CRGB leds[LED_COUNT];
+//время удержания кнопки для регистрации длинного нажатия (default - 150)
+#define HOLD_DURATION 150
 
 //НАСТРОИВАЕМЫЕ ПАРАМЕТРЫ:=========================================================
 // задержка выключения [ms] (default 180 000ms = 30min)
@@ -31,7 +33,7 @@ struct CRGB leds[LED_COUNT];
 unsigned long start = 0;
 // гистерезис функции debounce
 #define DEBOUNCE_DELAY 50
-
+volatile int pressTimer = 0;
 //переменные для poll()............................................................
 // время выключения
 unsigned long shutDown = 0;
@@ -44,8 +46,8 @@ unsigned long pulseStart;
 byte pulseFade = 0;
 byte pulseFadeStep = 0;
 unsigned long pulseStartTime = millis();
-
-
+int tmp = 0;
+int mode = 1;
 //FLAGS: ==========================================================================
 // флаг для buttonRoutine()
 bool timerFlag = false;
@@ -54,10 +56,11 @@ byte buttonState;
 byte *c;
 bool reset = false;
 
+// volatile unsigned long tmp = 0;
+
 // Иннициаллизация функций ========================================================
 void interrupt();
 bool debounce();
-void buttonRoutine();
 byte *Wheel(byte WheelPos);
 void heat();
 void pwrDown();
@@ -65,7 +68,11 @@ void pulse();
 void poll();
 void fadeOut();
 void kill();
-void animation1();
+void animation1(byte speedMultiplier);
+void animation2(byte hueDelta, byte speedMultiplier);
+void resetCheck();
+void onButtonClick();
+void onButtonHold();
 // закрасить все диоды ленты в один цвет
 void one_color_all(int cred, int cgrn, int cblu)
 {
@@ -86,20 +93,55 @@ void setup()
   LEDS.addLeds<WS2811, LED_DT, GRB>(leds, LED_COUNT); // настрйоки для нашей ленты
   pinMode(BUTTON_PIN, INPUT_PULLUP);                  // defaul HIGH
   pinMode(LED_PIN, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), interrupt, LOW); // triggers when LOW
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), interrupt, CHANGE); // triggers when LOW
   heat();
 }
 
 //LOOP ============================================================================
 void loop()
 {
- animation1();
+  if (mode == 1)
+  {
+    Serial.println("Animation in mode 1");
+    animation1(1);
+  }
+  else if (mode == 2)
+  {
+    Serial.println("Animation in mode 2");
+    animation1(5);
+  }
+  else if (mode == 3)
+  {
+    Serial.println("Animation in mode 3");
+    animation1(20);
+  }
+  else if (mode == 4)
+  {
+    Serial.println("Animation in mode 4");
+    animation2(3, 1);
+  }
+  else if (mode == 5)
+  {
+    Serial.println("Animation in mode 5");
+    animation2(3, 5);
+  }
+  else if (mode == 6)
+  {
+    Serial.println("Animation in mode 6");
+    animation2(3, 20);
+  }
+  else
+  {
+    Serial.println("defaul mode animation");
+    animation1(1);
+  }
 }
 
 //FUNCTIONS =======================================================================
-void animation1(){
- uint16_t i, j;
-  for (j = 0; j < 256 * 5; j++)
+void animation1(byte speedMultiplier)
+{
+  uint16_t i, j;
+  for (j = 0; j < 256 * 1; j++)
   { // 5 cycles of all colors on wheel
     for (i = 0; i < LED_COUNT; i++)
     {
@@ -112,7 +154,27 @@ void animation1(){
       poll();
     }
     FastLED.show();
-    delay(THIS_DELAY);
+    delay(THIS_DELAY / speedMultiplier);
+  }
+}
+
+void animation2(byte hueDelta, byte speedMultiplier)
+{
+  uint16_t i, j;
+  for (j = 0; j < 256 * 5; j++)
+  { // 5 cycles of all colors on wheel
+    for (i = 0; i < LED_COUNT; i++)
+    {
+      c = Wheel(((i * 256 / hueDelta) + j) & 255);
+      leds[i].setRGB(*c, *(c + 1), *(c + 2));
+    }
+    if (timerFlag == true)
+    {
+      pulse();
+      poll();
+    }
+    FastLED.show();
+    delay(THIS_DELAY / speedMultiplier);
   }
 }
 
@@ -180,70 +242,68 @@ void fadeOut()
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Button input related values
+static const int STATE_NORMAL = 0; // no button activity
+static const int STATE_SHORT = 1; // short button press
+static const int STATE_LONG = 2; // long button press
+volatile int resultButton = 0; // global value set by checkButton()
+
 void interrupt()
 {
-  if (!debounce())
-  {
+  /*
+  * This function implements software debouncing for a two-state button.
+  * It responds to a short press and a long press and identifies between
+  * the two states. Your sketch can continue processing while the button
+  * function is driven by pin changes.
+  */
+
+  const unsigned long LONG_DELTA = 500ul;              // hold seconds for a long press
+  const unsigned long DEBOUNCE_DELTA = 50ul;            // debounce time
+  static int lastButtonStatus = HIGH;                   // HIGH indicates the button is NOT pressed
+  int buttonStatus;                                     // button atate Pressed/LOW; Open/HIGH
+  static unsigned long longTime = 0ul, shortTime = 0ul; // future times to determine if button has been poressed a short or long time
+  boolean Released = true, Transition = false;          // various button states
+  boolean timeoutShort = false, timeoutLong = false;    // flags for the state of the presses
+
+  buttonStatus = digitalRead(BUTTON_PIN); // read the button state on the pin "BUTTON_PIN"
+  timeoutShort = (millis() > shortTime);  // calculate the current time states for the button presses
+  timeoutLong = (millis() > longTime);
+
+  if (buttonStatus != lastButtonStatus)
+  { // reset the timeouts if the button state changed
+    shortTime = millis() + DEBOUNCE_DELTA;
+    longTime = millis() + LONG_DELTA;
+  }
+
+  Transition = (buttonStatus != lastButtonStatus);   // has the button changed state
+  Released = (Transition && (buttonStatus == HIGH)); // for input pullup circuit
+
+  lastButtonStatus = buttonStatus; // save the button status
+
+  if (!Transition)
+  { //without a transition, there's no change in input
+    // if there has not been a transition, don't change the previous result
+    resultButton = STATE_NORMAL | resultButton;
     return;
   }
-  else
-  {
-    buttonRoutine();
-  }
-}
 
-bool debounce()
-{
-  unsigned long current = millis();
-  if (abs(current - start) > DEBOUNCE_DELAY)
-  {
-    start = millis();
-    return true;
+  if (timeoutLong && Released)
+  {                                           // long timeout has occurred and the button was just released
+    resultButton = STATE_LONG | resultButton; // ensure the button result reflects a long press
+    onButtonHold();
+  }
+  else if (timeoutShort && Released)
+  {                                            // short timeout has occurred (and not long timeout) and button was just released
+    resultButton = STATE_SHORT | resultButton; // ensure the button result reflects a short press
+    onButtonClick();
   }
   else
-  {
-    start = millis();
-    return false;
+  {                                             // else there is no change in status, return the normal state
+    resultButton = STATE_NORMAL | resultButton; // with no change in status, ensure no change in button status
   }
 }
-
-void buttonRoutine()
-{
-  if (reset == true)
-  {
-    reset = false;
-    asm volatile("  jmp 0");
-  }
-  if (buttonState == 0)
-  {
-    pulseFade = 0;
-    buttonState += 1;
-    timerFlag = true;
-    shutDown = millis() + TIMER_DELAY;
-    pulseDelay = 100;
-    pulseFadeStep = 2;
-  }
-  else if (buttonState == 1)
-  {
-    pulseFade = 0;
-    buttonState += 1;
-    timerFlag = true;
-    shutDown = millis() + TIMER_DELAY * 2;
-    pulseDelay = 1000;
-    pulseFadeStep = 10;
-  }
-  else if (buttonState == 2)
-  {
-    // cброс всех флажков
-    timerFlag = false;
-    analogWrite(LED_PIN, 0);
-    shutDown = 0;
-    pulseDelay = 0;
-    buttonState = 0;
-    pulseFade = 0;
-    pulseFadeStep = 0;
-  }
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pulse()
 {
@@ -278,9 +338,8 @@ void poll()
       pwrDown();
     }
   }
-  // Serial.print("До выключения (сек.) ");
-  // Serial.println((shutDown - millis()) / 1000);
-  // Serial.println(buttonState);
+  Serial.print("До выключения (сек.) ");
+  Serial.println((shutDown - millis()) / 1000);
 }
 
 void pwrDown()
@@ -301,3 +360,102 @@ void kill()
   sleep_enable();
   sleep_cpu();
 }
+
+void resetCheck()
+{
+  if (reset == true)
+  {
+    reset = false;
+    asm volatile("  jmp 0");
+  }
+}
+
+void onButtonClick()
+{
+  resetCheck();
+  if (buttonState == 0)
+  {
+    pulseFade = 0;
+    buttonState += 1;
+    timerFlag = true;
+    shutDown = millis() + TIMER_DELAY;
+    pulseDelay = 100;
+    pulseFadeStep = 2;
+  }
+  else if (buttonState == 1)
+  {
+    pulseFade = 0;
+    buttonState += 1;
+    timerFlag = true;
+    shutDown = millis() + TIMER_DELAY * 2;
+    pulseDelay = 1000;
+    pulseFadeStep = 10;
+  }
+  else if (buttonState == 2)
+  {
+    // cброс всех флажков
+    timerFlag = false;
+    analogWrite(LED_PIN, 0);
+    shutDown = 0;
+    pulseDelay = 0;
+    buttonState = 0;
+    pulseFade = 0;
+    pulseFadeStep = 0;
+  }
+}
+
+void onButtonHold()
+{
+  resetCheck();
+  mode++;
+  if (mode == 7)
+  {
+    mode = 1;
+  }
+  Serial.print("NOW MODE ");
+  Serial.println(mode);
+  for (size_t i = 255; i > 0; i--)
+  {
+    analogWrite(LED_PIN, i);
+    delay(10);
+  }
+  analogWrite(LED_PIN, 0);
+  loop();
+}
+// void interrupt()
+// {
+//   if (digitalRead(BUTTON_PIN) != HIGH)
+//   {
+//     tmp = millis();
+//   }
+
+//   if (!debounce())
+//   {
+//     return;
+//   }
+//   else
+//   {
+//     if ((abs(millis() - tmp)) >= HOLD_DURATION)
+//     {
+//       Serial.println((abs(millis() - tmp)));
+//       onButtonHold();
+//       tmp = millis();
+//       return;
+//     }
+//     onButtonClick();
+//   }
+// }
+// bool debounce()
+// {
+//   unsigned long current = millis();
+//   if (abs(current - start) > DEBOUNCE_DELAY)
+//   {
+//     start = millis();
+//     return true;
+//   }
+//   else
+//   {
+//     start = millis();
+//     return false;
+//   }
+// }
